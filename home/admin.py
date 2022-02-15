292271fce10f21012610018584dfd81acc87d4fb
Abdul Rehman Rais <74710518+abdulrehmanco@users.noreply.github.com>
2022-02-15 14:56:56 +0500

    Add files via upload

---
+++ home/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from.models import Company, Customer, Medicine, Bill, Employee, EmployeeSalary, User
+# Register your models here.
+admin.site.register(Company),
+admin.site.register(Customer),
+admin.site.register(Medicine),
+admin.site.register(Bill),
+admin.site.register(EmployeeSalary),
+admin.site.register(Employee),
+admin.site.register(User),
